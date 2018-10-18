0d8a783b6675f623dd2c025a4bd691c34d601bb6
Alex Bundy <abundy@opuslogica.com>
2018-10-18 10:39:50 -0700

    Fix deploy-dev when it's run without arguments.

--- deploy-dev.sh
+++ deploy-dev.sh
@@ -7,8 +7,10 @@ commit_hash="$2"
 
 if [[ -z "$deployment_url" ]]; then
     echo "./deploy-dev.sh <deployment_url> <commit_hash>"
-    local commit_hash=$(git rev-parse HEAD | cut -c1-7)
+    echo "(The <deployment_url> you want is probably \"docker.jobspeaker.com\")"
+    commit_hash=$(git rev-parse HEAD | cut -c1-7)
     echo "If you want to deploy with the current commit hash it is: $commit_hash"
+    exit 1
 fi
 
 set -u
